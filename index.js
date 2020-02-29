a6f72326a329522a88e28c5c3d2e72246e8d3e36
Katala121 <sanyafootb@list.ru>
2020-03-01 00:05:11 +0300

    created new branch homework_1

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const phoneBook = {};
+
 const readline = require('readline');
 
 const rl = readline.createInterface({ input: process.stdin , output: process.stdout });
@@ -14,17 +16,24 @@ const getLine = (function () {
 })();
 
 const main = async () => {
-    console.log('Введите команду:');
-    console.log('rectangle \t - для расчета площади прямоугольника');
-    console.log('circle \t\t - для расчета площади круга');
-    console.log('exit \t\t - для выхода');
-    const command = await getLine();
-    if (command === 'exit') {
-        process.exit(0);
-    }else if(command === 'rectangle'){
-        await calculateRectangleArea();
-    }else if(command === 'circle'){
-        await calculateCircleArea();
+    console.log('В вашем распоржении телефонная книга');
+    console.log('Используйте команды приведенные ниже:');
+    console.log('add - \t\tДля добавления контакта');
+    console.log('search - \tДля поиска контакта по имени');
+    console.log('del - \t\tДля удаления контакта');
+    console.log('view - \t\tДля просмотра всей телефонной книги');
+    console.log('exit - \t\tДля выхода');
+    const command = await getLine(); 
+       if (command === 'exit') {
+    process.exit(0);
+    }else if(command === 'add'){
+        await addContact();
+    }else if(command === 'view'){
+        await printPhoneBook();
+    }else if(command === 'search'){
+        await searchContact();
+    }else if(command === 'del'){
+        await deleteContact();
     } else {
         console.log('Неизвестная команда');
     }
@@ -34,20 +43,57 @@ const main = async () => {
 
 main();
  
-async function calculateRectangleArea(){
-    console.log('Введите высоту');
-    const height = Number(await getLine());
-    console.log('Введите ширину');
-    const width = Number(await getLine());
+async function addContact(){
+    console.log('Введите имя');
+    const contactName = String(await getLine());
+    console.log('Введите номер телефона');
+    const contactPhone = Number(await getLine());
 
-    const area = height * width;
-    console.log(`Площадь прямоугольника с высотой: ${height} и шириной: ${width} равна: ${area}`);
+    phoneBook[contactName] = contactPhone;
+    console.log(`Создан контакт: ${contactName}   ${contactPhone}`);
 }
 
-async function calculateCircleArea(){
-    console.log('Радиус круга');
-    const radius = Number(await getLine());
+async function printPhoneBook(){
+    if(Object.keys(phoneBook).length == 0){
+        console.log('В телефонной книге нет контактов\n');
+        return;
+    }
+    console.log('\t__ТЕЛЕФОННАЯ КНИГА__\n');
+    for(let key in phoneBook){
+        console.log(`\t${key}\t|\t${phoneBook[key]}`);
+    }
+}
 
-    const areaCircle = (radius * radius) * Math.PI;
-    console.log(`Площадь круга с радиусом: ${radius} равна: ${areaCircle}`);
+async function searchContact(){
+    if(Object.keys(phoneBook).length == 0){
+        console.log('В телефонной книге нет контактов\n');
+        return;
+    }
+    console.log('Введите имя для поиска');
+    const searchName = String(await getLine());
+    for(let key in phoneBook){
+        if(key == searchName){
+            console.log(`\t${key}\t|\t${phoneBook[key]}`);
+        }else{
+                console.log(`Нет контакта с именем ${searchName} в телефонной книге`);
+            }
+        }
+}                                  
+
+
+async function deleteContact(){
+    if(Object.keys(phoneBook).length == 0){
+        console.log('В телефонной книге нет контактов\n');
+        return;
+    }
+    console.log('Введите имя контакта');
+    const deleteName = String(await getLine());
+    for(let key in phoneBook){
+        if(key == deleteName){ 
+            delete phoneBook[key];
+            console.log(`контакт ${key} удален`);
+        }else{
+            console.log(`Нет контакта с именем ${deleteName} в телефонной книге`);
+        }
+    }
 }
