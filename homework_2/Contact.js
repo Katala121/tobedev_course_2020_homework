3bd4618c2f9be94f346d2275776b1467325b6304
Katala121 <sanyafootb@list.ru>
2020-03-09 01:07:53 +0300

    Change Contact.js

--- homework_2/Contact.js
+++ homework_2/Contact.js
@@ -1,57 +1,58 @@
 'use strict'
 const phoneBook = require('./PhoneBook');
-const consoleReader = require('./ConsoleReader');
-const isPhoneBookNotEmpty = require('./IsPhoneBookNotEmpty'); 
+const ConsoleReader = require('./ConsoleReader');
+const isPhoneBookNotEmpty = require('./IsPhoneBookNotEmpty');
+const massagePrinter = require('./MassagePrinter');
 
 async function addContact(){
-    console.log('Введите имя');
-    const contactName = String(await consoleReader.getLine());
-    console.log('Введите номер телефона');
-    const contactPhone = Number(await consoleReader.getLine());
+    massagePrinter.printTooltip('Введите имя');
+    const contactName = String(await ConsoleReader.getLine());
+    massagePrinter.printTooltip('Введите номер телефона');
+    const contactPhone = Number(await ConsoleReader.getLine());
 
     phoneBook[contactName] = contactPhone;
-    console.log(`Создан контакт: ${contactName}   ${contactPhone}`);
+    massagePrinter.printMassage(`Создан контакт: ${contactName}   ${contactPhone}`);
 }
 
 async function printPhoneBook(){
     if( isPhoneBookNotEmpty() ){ 
-        console.log('\t__ТЕЛЕФОННАЯ КНИГА__\n');
+        massagePrinter.printMassage('\t__ТЕЛЕФОННАЯ КНИГА__\n');
         for(let key in phoneBook){
-            console.log(`\t${key}\t|\t${phoneBook[key]}`);
+            massagePrinter.printMassage(`\t${key}\t|\t${phoneBook[key]}`);
         }
     }else{
-        console.log('\tТелефонная книга пуста!');
+        massagePrinter.printError('\tТелефонная книга пуста!');
     }
 }
 
 async function searchContact(){
     if( isPhoneBookNotEmpty()){ 
-        console.log('Введите имя для поиска');
-        const searchName = String(await consoleReader.getLine());
+        massagePrinter.printTooltip('Введите имя для поиска');
+        const searchName = String(await ConsoleReader.getLine());
         
         if(searchName in phoneBook){
-            console.log(`\t${searchName}\t|\t${phoneBook[searchName]}`);
+            massagePrinter.printMassage(`\t${searchName}\t|\t${phoneBook[searchName]}`);
         }else{
-            console.log(`Нет контакта с именем ${searchName} в телефонной книге`);
+            massagePrinter.printError(`Нет контакта с именем ${searchName} в телефонной книге`);
         }
     }else{
-        console.log('\tТелефонная книга пуста!');
+        massagePrinter.printError('\tТелефонная книга пуста!');
     }
     
 }                                  
 
 async function deleteContact(){
     if( isPhoneBookNotEmpty()){   
-        console.log('Введите имя контакта');
-        const deleteName = String(await consoleReader.getLine());
+        massagePrinter.printTooltip('Введите имя контакта');
+        const deleteName = String(await ConsoleReader.getLine());
         if(deleteName in phoneBook){
             delete phoneBook[deleteName];
-            console.log(`контакт ${deleteName} удален`);
+            massagePrinter.printMassage(`контакт ${deleteName} удален`);
         }else{
-            console.log(`Нет контакта с именем ${deleteName} в телефонной книге`);
+            massagePrinter.printError(`Нет контакта с именем ${deleteName} в телефонной книге`);
         }
     }else{
-        console.log('\tТелефонная книга пуста!');
+        massagePrinter.printError('\tТелефонная книга пуста!');
     }
 }
 
